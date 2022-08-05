4c473e3f60c51d2d18d28bbf16a1fa70a69142f6
ayuputrii <ayuarmadani.pm@gmail.com>
2022-08-05 11:38:38 +0700

    Update db

--- config/db.php
+++ config/db.php
@@ -6,17 +6,4 @@
   }else{
     echo "";
   }
-
-  // $servername = 'localhost:3306';
-  // $username = 'root';
-  // $password = '';
-
-  // try {
-  //   $conn = new PDO("mysql:host=$servername;dbname=uas_ayu", $username, $password);
-  //   // set the PDO error mode to exception
-  //   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //   echo "";
-  // } catch(PDOException $e) {
-  //   echo "Connection failed: " . $e->getMessage();
-  // }
 ?>
